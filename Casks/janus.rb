03acbb04e89e1457adedece82dd5e2a3d7e3312b
GitHub Action <action@github.com>
2026-03-27 01:02:36 +0000

    Update janus to v1.19.0

--- Casks/janus.rb
+++ Casks/janus.rb
@@ -1,12 +1,12 @@
 cask "janus" do
-  version "1.18.0"
-  sha256 "bc0ac8d0df4e7433a4607760462f40964dae9a8e72f6461d2f8cc95e39871f94"
+  version "1.19.0"
+  sha256 "93d89b541d2ccb18e0a9fde2684ce706c190f59cb176ba7ccdf12d92518cfec1"
 
   desc "Plain-text issue tracking CLI with Markdown files and remote sync"
   homepage "https://github.com/divmain/janus"
   
   # URL will be dynamically updated by automation
-  url "https://github.com/divmain/homebrew-janus/releases/download/v1.18.0/janus-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/divmain/homebrew-janus/releases/download/v1.19.0/janus-aarch64-apple-darwin.tar.gz"
     
   depends_on macos: ">= :monterey"
   depends_on arch: :arm64
